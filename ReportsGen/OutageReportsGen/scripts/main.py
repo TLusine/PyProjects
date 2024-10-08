10887501e09bc72359229eb0e76a100b6544a385
TLusine <lusineshahbazyan@gmail.com>
2024-10-08 12:24:40 +0400

    Modified for GUI

--- ReportsGen/OutageReportsGen/scripts/main.py
+++ ReportsGen/OutageReportsGen/scripts/main.py
@@ -1,19 +1,37 @@
 import subprocess
+import logging
+import report_builder_gui
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Run 1st script
-subprocess.run(['python', 'problem_descriptions.py'])
+logging.info("\nRunning copy_addresses.py...")
+subprocess.run(['python', 'copy_addresses.py'])
 
 # Run 2nd script
-subprocess.run(['python', 'U_L_extensions.py'])
-
-# Run 3rd script
+logging.info("\nRunning address_extensions.py...")
 subprocess.run(['python', 'address_extensions.py'])
 
+
+# Run 3rd script
+logging.info("Running problem_descriptions.py...")
+subprocess.run(['python', 'problem_descriptions.py'])
+
 # Run 4th script
+logging.info("\nRunning U_L_extensions.py...")
+subprocess.run(['python', 'U_L_extensions.py'])
+
+# Run 5th script
+logging.info("\nRunning summary_report.py...")
 subprocess.run(['python', 'summary_report.py'])
 
-# Run 5th script, full copy/paste value DB
+# Run 6th script, full copy/paste value DB
+logging.info("\nRunning copy_summary_report.py...")
 subprocess.run(['python', 'copy_summary_report.py'])
 
-# Run 6th, full copy/paste value DB
-subprocess.run(['python', 'generate_outage_report.py'])
+# Launch GUI for report builder
+logging.info("\nLaunching GUI (report_builder_gui.py)...")
+report_builder_gui.main()  # Call the main function of the GUI directly
+
+logging.info("Main script complete.")
